63057c1d12d5e46d8d37e7bd3101d1c372c9c009
Fimofey <43012401+DEEINT@users.noreply.github.com>
2021-11-24 14:56:06 +0500

    Были исправленны замечания по PEP8.

--- Filippov_Timofey_Home_Work/filter.py
+++ Filippov_Timofey_Home_Work/filter.py
@@ -1,38 +1,41 @@
 from PIL import Image
 import numpy as np
 
-def Find_medS(arr, i, j, mosSize):
+
+def Find_medS(array, i, j, size):
     """Определение средней освященности в группе пикселей
 
-    >>>Find_medS(arr, i, j, mosSize)
+    >>>Find_medS(array, i, j, size)
     142
 
     """
     medS = 0
-    for n in range(i, i + mosSize):
-        for n1 in range(j, j + mosSize):
-            red = arr[n][n1][0]
-            green = arr[n][n1][1]
-            blue = arr[n][n1][2]
-            pixel = (int(red) + int(blue) + int(green))/3
+    for n in range(i, i + size):
+        for n1 in range(j, j + size):
+            red = array[n][n1][0]
+            green = array[n][n1][1]
+            blue = array[n][n1][2]
+            pixel = (int(red) + int(blue) + int(green)) / 3
             medS += pixel
     medS = int(medS // 100)
     return medS
 
-def black_white(arr, grayscale, height, inp, mosSize, width):
-    """Присвоение заданному колличеству пикселей нужной градации серго"""
-    for i in range(0, height - 1, mosSize):
-        for j in range(0, width - 1, mosSize):
-            medS = Find_medS(arr, i, j, mosSize)
-            for n in range(i, i + mosSize):
-                for n1 in range(j, j + mosSize):
-                    arr[n][n1][0] = int(medS // grayscale) * grayscale
-                    arr[n][n1][1] = int(medS // grayscale) * grayscale
-                    arr[n][n1][2] = int(medS // grayscale) * grayscale
-    res = Image.fromarray(arr)
-    res.save(inp[1])
 
-print('Введите полное имя исходного изображения и результата.',
+def black_white(array, scale, height, input, size, width):
+    """Присвоение заданному колличеству пикселей нужной градации серго"""
+    for i in range(0, height - 1, size):
+        for j in range(0, width - 1, size):
+            medS = Find_medS(array, i, j, size)
+            for n in range(i, i + size):
+                for n1 in range(j, j + size):
+                    array[n][n1][0] = int(medS // scale) * scale
+                    array[n][n1][1] = int(medS // scale) * scale
+                    array[n][n1][2] = int(medS // scale) * scale
+    res = Image.fromarray(array)
+    res.save(input[1])
+
+
+print('Введите полное имя исходного изображения и результата. '
       'Размер мозайки. Градацию серого. (разделитель - " ")')
 inp = input().split(' ')
 img = Image.open(inp[0])
